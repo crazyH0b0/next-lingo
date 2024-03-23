b45fb21dd0c7e7f805ef7b439874cbc016b1c23b
6666 <1327185450@qq.com>
2024-03-23 23:46:52 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full flex items-center justify-center h-screen">
+      <Button>1231</Button>
+    </div>
+  );
+}
