c43f1fd4c4e273862de846445c6aa3c5003f779c
6666 <1327185450@qq.com>
2024-04-13 17:07:53 +0800

    完善功能

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -14,17 +14,14 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6 ">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{
-            title: 'Spanish',
-            imageSrc: '/es.svg',
-          }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveDesc={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
         {/* <div className="h-[3000px]  w-full"></div> */}
       </FeedWrapper>
     </div>
--- db/schema.ts
+++ db/schema.ts
@@ -7,7 +7,7 @@ export const courses = pgTable('courses', {
   imageSrc: text('image_src').notNull(),
 });
 
-// userProgress <-> courses 多对多
+// 这表示 courses 到 userProgress 的一对多关系。每个课程可以与多个用户进度记录相关联。
 export const coursesRelations = relations(courses, ({ many }) => ({
   userProgress: many(userProgress),
   // units: many(units),
@@ -22,7 +22,9 @@ export const userProgress = pgTable('user_progress', {
   points: integer('points').notNull().default(0),
 });
 
-//  userProgress -> activeCourse 一对多
+//  这建立了 userProgress 和 courses 之间的一对一关系。
+//  fields 和 references 数组将 userProgress.activeCourseId 与 courses.id 配对，
+//  表示每个 userProgress 记录通过其 activeCourseId 与确切一个课程链接。
 export const userProgressRelations = relations(userProgress, ({ one }) => ({
   activeCourse: one(courses, {
     fields: [userProgress.activeCourseId],
